1369f582ee8221d33565bd3388d4f111430a5bf7
reklezz <drelixgaming@gmail.com>
2018-02-01 11:36:10 +0100

    Pushing Accumulator and divisor

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -8,25 +8,54 @@ namespace Calculator
 {
     public class Calculator
     {
+        public double Accumulator { get; private set; } = 0;
+
+
         public double Add(double a, double b)
         {
-            return a + b;
+            Accumulator = a + b;
+            return Accumulator;
 
         }
 
         public double Subtract(double a, double b)
         {
-            return a - b;
+            Accumulator = a - b;
+
+            return Accumulator;
         }
 
         public double Multiply(double a, double b)
         {
-            return a * b;
+            Accumulator = a - b;
+            return Accumulator;
         }
 
         public double Power(double x, double exp)
         {
-            return Math.Pow(x, exp);
+            if (exp < 0)
+            {
+                throw new InvalidOperationException("You used a negative exponent");
+            }
+            Accumulator = Math.Pow(x, exp);
+            return Accumulator;
+        }
+
+        public double Divide(double dividend, double divisor)
+        {
+            if (dividend == 0 || divisor == 0)
+            {
+                throw new DivideByZeroException("You divided by zero");
+            }
+
+            Accumulator = dividend / divisor;
+
+            return Accumulator;
+        }
+
+        public void Clear()
+        {
+            Accumulator = 0;
         }
     }
 }
