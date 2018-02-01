c4d059d065fb9efaa15e427155d4560227ef50c0
StefanieNielson <nielson1996@hotmail.com>
2018-02-01 10:56:21 +0100

    Add project files.

---
+++ Calculator.UT/CalculatorUnitTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Internal.Execution;
+
+namespace Calculator.UT
+{
+    [TestFixture]
+    public class CalculatorUnitTests
+    {
+        [Test]
+        public void Add_Add1And2_Returns3()
+        {
+            var uut = new Calculator();
+
+            Assert.That(uut.Add(1,2), Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Add_Add0And2_Returns2()
+        {
+            var uut = new Calculator();
+
+            Assert.That(uut.Add(0,2),Is.EqualTo(2));
+        }
+
+        [TestCase(1, 3, -2)]
+        [TestCase(-1, -3, 2)]
+        public void Subtract_SimpleCalculations(double a, double b, double expected)
+        {
+            var uut = new Calculator();
+
+            Assert.That(uut.Subtract(a, b), Is.EqualTo(expected));
+        }
+    }
+}
---
+++ Exercise1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    //Exercise 1: Implement class Calculator
+    class Calculator
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+     
+        }
+
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Power(double x, double exp)
+        {
+            return Math.Pow(x, exp);
+        }
+    }
+
+    //Exercise 2: Test the class - Explaining is in OneNote
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator newCalculator = new Calculator();
+
+            double sum = newCalculator.Add(2, 5);
+            Console.WriteLine(sum);                     //7
+
+            double sum1 = newCalculator.Add('b', 3);    
+            Console.WriteLine(sum1);                    //error - Forkert -> Bliver 101 da den tager ASCII værdien for b = 98
+
+
+            double sum2 = newCalculator.Add(-4, 3);
+            Console.WriteLine(sum2);                    //-1
+
+            double sum3 = newCalculator.Add(-3, -6);
+            Console.WriteLine(sum3);                    //-9
+
+            double sum4 = newCalculator.Add(1111111111, 1);
+            Console.WriteLine(sum4);                    //1111111112
+
+        }
+    }
+
+ 
+}
+
+
